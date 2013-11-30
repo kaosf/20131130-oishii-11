a0971c16c9f2ed6b6697734ecf69f3f1cec32c5a
ka <ka.kaosf@gmail.com>
2013-11-30 15:32:55 +0900

    implement Message class

--- src/com/example/oishii11/Message.java
+++ src/com/example/oishii11/Message.java
@@ -1,5 +1,26 @@
 package com.example.oishii11;
 
-public class Message {
+import java.util.Random;
 
+public class Message {
+	int id;
+	String body;
+
+	public Message(String body) {
+		this.id = new Random(System.currentTimeMillis()).nextInt();
+		this.body = body;
+	}
+
+	public Message(int id, String body) {
+		this.id = id;
+		this.body = body;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getBody() {
+		return body;
+	}
 }
