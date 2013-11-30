e2e54b93f115b7b6c1a676a7602cb25e63ff3214
ka <ka.kaosf@gmail.com>
2013-11-30 14:59:01 +0900

    write code template for setOnKeyListener

--- src/com/example/oishii11/MainActivity.java
+++ src/com/example/oishii11/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.oishii11;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
@@ -13,6 +16,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         EditText edit = (EditText)findViewById(R.id.editText1);
+        edit.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
     }
 
 
